b76328282b0ef5a78d5e8942a4cb6fb1254fa880
thaslii <thasleemarafeek2003@gmail.com>
2023-10-25 12:24:06 +0530

    first commit

---
+++ ecoapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+app_name='ecoapp'
+
+urlpatterns = [
+
+    path('',views.allProdCat,name='allProdCat'),
+    path('<slug:c_slug>/',views.allProdCat,name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='prodCatdetail'),
+]
